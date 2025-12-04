b2f0b9093e4ec209b026567ccb7980fca8d5be32
mohamed mahmoud <bnaboabdallah@gmail.com>
2025-12-04 04:43:58 +0200

    add donation link for who wanna to support me

--- src/veiw/components/Donate.jsx
+++ src/veiw/components/Donate.jsx
@@ -1,9 +1,15 @@
 import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Donate = () => {
-  return <button className="btn btn-black">donate <FontAwesomeIcon icon={faArrowUpRightFromSquare} /></button>;
+  return (
+    <a className="btn btn-black" href="https://buymeacoffee.com/mohamedctrl">
+      <button className=" btn-black">Donate</button>
+      <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+    </a>
+  );
 };
 
 export default Donate;
